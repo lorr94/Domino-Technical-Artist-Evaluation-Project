7b4ca1b065d81931d9be6fe1fea9b88a9ac06cb2
lorr94 <125183429+lorr94@users.noreply.github.com>
2025-10-13 16:09:34 +0300

    Added new packs for Atlases and created a StickerData ScriptableObject script
    - reloaded pack and added new relavant one for Main Window Atlas
    - StickerData script
    - Deleted old UI - Image object in Hierarchy

---
+++ Assets/Scripts/StickerData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewStickerData", menuName = "Sticker System/Sticker Data")]
+public class StickerData : ScriptableObject
+{
+
+    public int crownIndex = 1;
+
+    public string stickerName = "STICKER NAME";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
