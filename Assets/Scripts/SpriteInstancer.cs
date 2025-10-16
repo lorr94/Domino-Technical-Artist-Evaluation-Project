6955a7984e26bfcc0a7dc546735b3edfc531b4cb
lorr94 <kobaini94@gmail.com>
2025-10-17 01:22:17 +0300

    rearranged UI elements in Hierarchy

---
+++ Assets/Scripts/SpriteInstancer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+public class SpriteInstancer : MonoBehaviour
+{
+
+    [SerializeField]
+    private SpriteAtlas _spriteAtlas;
+    [SerializeField]
+    private string _spriteName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().sprite = _spriteAtlas.GetSprite(_spriteName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
